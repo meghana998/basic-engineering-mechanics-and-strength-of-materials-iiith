99e493d8235e4b6cb4621aa865fdd411df44db69
matta.meghana@gmail.com <matta.meghana@gmail.com>
2019-07-12 23:36:21 +0530

    added some code

---
+++ SRIP/Codes/jsfile.js
@@ -0,0 +1,72 @@
+            var slider1 = document.getElementById("myRange1");
+			var output1 = document.getElementById("demo1");
+			output1.innerHTML = slider1.value;
+			slider1.oninput = function() {
+ 				 output1.innerHTML = this.value;
+				}
+			var slider2 = document.getElementById("myRange2");
+			var output2 = document.getElementById("demo2");
+			output2.innerHTML = slider2.value;
+			slider2.oninput = function() {
+ 				 output2.innerHTML = this.value;
+				}
+			var slider3 = document.getElementById("myRange3");
+			var output3= document.getElementById("demo3");
+			output3.innerHTML = slider3.value;
+			slider3.oninput = function() {
+ 				 output3.innerHTML = this.value;
+				}
+			var slider4 = document.getElementById("myRange4");
+			var output4 = document.getElementById("demo4");
+			output4.innerHTML = slider4.value;
+			slider4.oninput = function() {
+ 				 output4.innerHTML = this.value;
+				}
+			var slider5 = document.getElementById("myRange5");
+			var output5 = document.getElementById("demo5");
+			output5.innerHTML = slider5.value;
+			slider5.oninput = function() {
+ 				 output5.innerHTML = this.value;
+				}
+			var slider6 = document.getElementById("myRange6");
+			var output6 = document.getElementById("demo6");
+			output6.innerHTML = slider6.value;
+			slider6.oninput = function() {
+ 				 output6.innerHTML = this.value;
+				}
+			var slider7 = document.getElementById("myRange7");
+			var output7 = document.getElementById("demo7");
+			output7.innerHTML = slider7.value;
+			slider7.oninput = function() {
+ 				 output7.innerHTML = this.value;
+				}
+			var slider8 = document.getElementById("myRange8");
+			var output8 = document.getElementById("demo8");
+			output8.innerHTML = slider8.value;
+			slider8.oninput = function() {
+ 				 output8.innerHTML = this.value;
+				}
+			var slider9 = document.getElementById("myRange9");
+			var output9 = document.getElementById("demo9");
+			output9.innerHTML = slider9.value;
+			slider9.oninput = function() {
+ 				 output9.innerHTML = this.value;
+				}
+			var slider10 = document.getElementById("myRange10");
+			var output10 = document.getElementById("demo10");
+			output10.innerHTML = slider10.value;
+			slider10.oninput = function() {
+ 				 output10.innerHTML = this.value;
+				}
+			var slider11 = document.getElementById("myRange11");
+			var output11 = document.getElementById("demo11");
+			output11.innerHTML = slider11.value;
+			slider11.oninput = function() {
+ 				 output11.innerHTML = this.value;
+				}
+			var slider12 = document.getElementById("myRange12");
+			var output12 = document.getElementById("demo12");
+			output12.innerHTML = slider12.value;
+			slider12.oninput = function() {
+ 				 output12.innerHTML = this.value;
+				}
